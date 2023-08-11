d768092b4e301e8eccb92e3127205bcefaff5013
Miro <70092715+529565622@users.noreply.github.com>
2023-08-11 15:34:55 +0800

    Update BiliAnalysis.user.js

--- BiliAnalysis.user.js
+++ BiliAnalysis.user.js
@@ -10,11 +10,11 @@
 // @grant        GM_xmlhttpRequest
 // @grant        GM_notification
 // @run-at       document-idle
+// @require           https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.2.1/jquery.min.js
 // ==/UserScript==
 //20230405 修复解析1080p(需已登陆)
 //20230626 修复加载慢导致无法添加按钮
 //20230811 添加右下角解析按钮 加快按钮出现速度
-
 (function () {
     'use strict';
     var button = document.createElement("button")
@@ -27,11 +27,14 @@
     button.style.borderRadius = "6px"
     button.style.fontSize = '14px'
     button.addEventListener("click", clickBotton)
+    var BiliAnalysisbutton = `<button id="BiliAnalysis" style="width: 20px; color: rgb(255, 255, 255); background: rgb(0, 174, 236); border: 1px solid rgb(241, 242, 243); border-radius: 6px; font-size: 14px;top:800px;right:0px;position:fixed;">本地解析</button>`;
+    $("body").append(BiliAnalysisbutton)
+    document.getElementById('BiliAnalysis').addEventListener('click', clickBotton)
     setTimeout(function () {
         var like_comment = document.getElementsByClassName('left-entry')[0]
         like_comment.appendChild(button)
     }, 5000)
-        setTimeout(function () {
+    setTimeout(function () {
         var like_comment = document.getElementsByClassName('left-entry')[0]
         like_comment.appendChild(button)
     }, 10000)
@@ -42,7 +45,7 @@
         var BV1 = url.match(BV)
         var P1 = url.match(P)
         if (P1 == null) {
-        P1 = 1
+            P1 = 1
         }
         var httpRequest = new XMLHttpRequest();
         httpRequest.open('GET', 'https://api.bilibili.com/x/player/pagelist?bvid=' + BV1, true);
@@ -50,10 +53,10 @@
         httpRequest.onreadystatechange = function () {
             if (httpRequest.readyState == 4 && httpRequest.status == 200) {
                 var json = JSON.parse(httpRequest.responseText);
-                var cid = json.data[P1-1].cid
-                console.log(json.data[P1-1].cid);
+                var cid = json.data[P1 - 1].cid
+                console.log(json.data[P1 - 1].cid);
                 var httpRequest1 = new XMLHttpRequest();
-                httpRequest1.open('GET', 'https://api.bilibili.com/x/player/playurl?bvid=' + BV1 + '&cid=' + cid + '&qn=64&type=&otype=json&platform=html5&high_quality=1', true);
+                httpRequest1.open('GET', 'https://api.bilibili.com/x/player/playurl?bvid=' + BV1 + '&cid=' + cid + '&qn=116&type=&otype=json&platform=html5&high_quality=1', true);
                 httpRequest1.withCredentials = true;
                 httpRequest1.send();
                 httpRequest1.onreadystatechange = function () {
