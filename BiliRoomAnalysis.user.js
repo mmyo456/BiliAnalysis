e66fd982142a892d56a571f66599074478b688bf
Miro <70092715+529565622@users.noreply.github.com>
2023-08-11 21:45:22 +0800

    Update BiliRoomAnalysis.user.js

--- BiliRoomAnalysis.user.js
+++ BiliRoomAnalysis.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BiliBili直播间本地解析(Miro)
 // @namespace    https://bbs.tampermonkey.net.cn/
-// @version      0.1.0
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       Miro(https://vrchat.com/home/user/usr_20b8e0e4-9e16-406a-a61d-8a627ec1a2e3)
 // @match        https://live.bilibili.com/*
@@ -10,6 +10,7 @@
 // @grant        GM_xmlhttpRequest
 // @grant        GM_notification
 // @run-at       document-idle
+// @require      https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.2.1/jquery.min.js
 // ==/UserScript==
 
 (function () {
@@ -24,31 +25,34 @@
     button.style.borderRadius = "6px"
     button.style.fontSize = '14px'
     button.addEventListener("click", clickBotton)
+    var BiliAnalysisbutton = `<button id="BiliAnalysis" style="z-index:999; width: 50px; height:50px; color: rgb(255, 255, 255); background: rgb(0, 174, 236); border: 1px solid rgb(241, 242, 243); border-radius: 6px; font-size: 14px;top:500px;right:0px;position:fixed;">本地解析</button>`;
+    $("body").append(BiliAnalysisbutton)
+    document.getElementById('BiliAnalysis').addEventListener('click', clickBotton)
     setTimeout(function () {
         var like_comment = document.getElementsByClassName('flex-block')[0]
         like_comment.appendChild(button)
     }, 5000)
-        setTimeout(function () {
+    setTimeout(function () {
         var like_comment = document.getElementsByClassName('flex-block')[0]
         like_comment.appendChild(button)
     }, 10000)
     function clickBotton() {
         var url = window.location.href
-        var Roomid=/(?<=com\/).*?(?=\?)/
+        var Roomid = /(?<=com\/).*?(?=\?)/
         var Roomid1 = url.match(Roomid)
         var httpRequest = new XMLHttpRequest();
-        httpRequest.open('GET', 'https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id='+Roomid1+'&protocol=0,1&format=0,1,2&codec=0,1&qn=10000&platform=web&ptype=8&dolby=5&panorama=1', true);
+        httpRequest.open('GET', 'https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id=' + Roomid1 + '&protocol=0,1&format=0,1,2&codec=0,1&qn=10000&platform=web&ptype=8&dolby=5&panorama=1', true);
         httpRequest.send();
         httpRequest.onreadystatechange = function () {
             if (httpRequest.readyState == 4 && httpRequest.status == 200) {
                 var json = JSON.parse(httpRequest.responseText);
-                var host = json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].host
+                var host = json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].host
                 console.log(host);
-                var baseurl=json.data.playurl_info.playurl.stream[1].format[1].codec[0].base_url
+                var baseurl = json.data.playurl_info.playurl.stream[1].format[0].codec[0].base_url
                 console.log(baseurl);
-                var extra=json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].extra
+                var extra = json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].extra
                 console.log(extra);
-                var roomurl=host+baseurl+extra
+                var roomurl = host + baseurl + extra
                 navigator.clipboard.writeText(roomurl).catch(e => console.error(e))
                 GM_notification({
                     title: "解析成功",
