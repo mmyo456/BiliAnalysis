b0718866222751c037fdab1c1eeade8a022e48fa
Miro <70092715+529565622@users.noreply.github.com>
2023-11-30 19:55:55 +0800

    Update BiliRoomAnalysis.user.js

--- BiliRoomAnalysis.user.js
+++ BiliRoomAnalysis.user.js
@@ -13,73 +13,73 @@
 // ==/UserScript==
 
 (function () {
-    'use strict';
-    var button = document.createElement("button")
-    button.textContent = "本地解析"
-    button.style.width = "80px"
-    button.style.align = "center"
-    button.style.color = "#FFFFFF"
-    button.style.background = "#00AEEC"
-    button.style.border = "1px solid #F1F2F3"
-    button.style.borderRadius = "6px"
-    button.style.fontSize = '14px'
-    button.addEventListener("click", clickBotton)
-    var BiliAnalysisbutton = `<button id="BiliAnalysis" style="z-index:999; width: 50px; height:50px; color: rgb(255, 255, 255); background: rgb(0, 174, 236); border: 1px solid rgb(241, 242, 243); border-radius: 6px; font-size: 14px;top:500px;right:0px;position:fixed;">本地解析</button>`;
-    $("body").append(BiliAnalysisbutton)
-    document.getElementById('BiliAnalysis').addEventListener('click', clickBotton)
-    var BiliAnalysisbutton1 = `<button id="BiliAnalysis1" style="z-index:999; width: 50px; height:50px; color: rgb(255, 255, 255); background: rgb(0, 174, 236); border: 1px solid rgb(241, 242, 243); border-radius: 6px; font-size: 14px;top:100px;left:0px;position:fixed;">本地解析</button>`;
-    $("body").append(BiliAnalysisbutton1)
-    document.getElementById('BiliAnalysis1').addEventListener('click', clickBotton)
-    setTimeout(function () {
-        var like_comment = document.getElementsByClassName('flex-block')[0]
-        like_comment.appendChild(button)
-    }, 5000)
-    setTimeout(function () {
-        var like_comment = document.getElementsByClassName('flex-block')[0]
-        like_comment.appendChild(button)
-    }, 10000)
-    function clickBotton() {
-        var url = window.location.href
-        var Roomid = /(?<=com\/).*?(?=\?)/
-        var Roomid1 = url.match(Roomid)
-        var httpRequest = new XMLHttpRequest();
-        httpRequest.open('GET', 'https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id=' + Roomid1 + '&protocol=0,1&format=0,1,2&codec=0,1&qn=10000&platform=web&ptype=8&dolby=5&panorama=1', true);
-        httpRequest.send();
-        httpRequest.onreadystatechange = function () {
-            if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-                var json = JSON.parse(httpRequest.responseText);
-                var host = "";
-                var baseurl = "";
-                var extra = "";
-                var roomurl
-                if (json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].host) {
-                  host = json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].host;
-                  baseurl = json.data.playurl_info.playurl.stream[1].format[1].codec[0].base_url;
-                  extra = json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].extra;
-                } else if (json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].host) {
-                  host = json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].host;
-                  baseurl = json.data.playurl_info.playurl.stream[1].format[0].codec[0].base_url;
-                  extra = json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].extra;
-                } else if (json.data.playurl_info.playurl.stream[0].format[0].codec[0].url_info[0].host) {
-                  host = json.data.playurl_info.playurl.stream[0].format[0].codec[0].url_info[0].host;
-                  baseurl = json.data.playurl_info.playurl.stream[0].format[0].codec[0].base_url;
-                  extra = json.data.playurl_info.playurl.stream[0].format[0].codec[0].url_info[0].extra;
-                }
-                roomurl = host + baseurl + extra;
-                navigator.clipboard.writeText(roomurl).catch(e => console.error(e))
-                GM_notification({
-                    title: "解析成功",
-                    image: "https://testingcf.jsdelivr.net/gh/mmyo456/BiliAnalysis-1@main/img/6.jpg",
-                    text: "解析成功",
-                    highlight: true,
-                    silent: false,
-                    timeout: 10000,
-                    onclick: function () {
-                    },
-                    ondone() {
-                    }
-                })
-            }
-        };
-    }
+  'use strict';
+  var button = document.createElement("button")
+  button.textContent = "本地解析"
+  button.style.width = "80px"
+  button.style.align = "center"
+  button.style.color = "#FFFFFF"
+  button.style.background = "#00AEEC"
+  button.style.border = "1px solid #F1F2F3"
+  button.style.borderRadius = "6px"
+  button.style.fontSize = '14px'
+  button.addEventListener("click", clickBotton)
+  var BiliAnalysisbutton = `<button id="BiliAnalysis" style="z-index:999; width: 45px;height:45px;color: rgb(255, 255, 255); background: rgb(0, 174, 236); border: 1px solid rgb(241, 242, 243); border-radius: 6px; font-size: 14px;top:850px;right:0px;position:fixed;">本地</br>解析</button>`;
+  $("body").append(BiliAnalysisbutton)
+  document.getElementById('BiliAnalysis').addEventListener('click', clickBotton)
+  var BiliAnalysisbutton1 = `<button id="BiliAnalysis1" style="z-index:999; width: 45px;height:45px;color: rgb(255, 255, 255); background: rgb(0, 174, 236); border: 1px solid rgb(241, 242, 243); border-radius: 6px; font-size: 14px;top:200px;left:0px;position:fixed;">本地</br>解析</button>`;
+  $("body").append(BiliAnalysisbutton1)
+  document.getElementById('BiliAnalysis1').addEventListener('click', clickBotton)
+  setTimeout(function () {
+    var like_comment = document.getElementsByClassName('flex-block')[0]
+    like_comment.appendChild(button)
+  }, 5000)
+  setTimeout(function () {
+    var like_comment = document.getElementsByClassName('flex-block')[0]
+    like_comment.appendChild(button)
+  }, 10000)
+  function clickBotton() {
+    var url = window.location.href
+    var Roomid = /(?<=com\/).*?(?=\?)/
+    var Roomid1 = url.match(Roomid)
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', 'https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id=' + Roomid1 + '&protocol=0,1&format=0,1,2&codec=0,1&qn=10000&platform=web&ptype=8&dolby=5&panorama=1', true);
+    httpRequest.send();
+    httpRequest.onreadystatechange = function () {
+      if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+        var json = JSON.parse(httpRequest.responseText);
+        var host = "";
+        var baseurl = "";
+        var extra = "";
+        var roomurl
+        if (json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].host) {
+          host = json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].host;
+          baseurl = json.data.playurl_info.playurl.stream[1].format[1].codec[0].base_url;
+          extra = json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].extra;
+        } else if (json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].host) {
+          host = json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].host;
+          baseurl = json.data.playurl_info.playurl.stream[1].format[0].codec[0].base_url;
+          extra = json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].extra;
+        } else if (json.data.playurl_info.playurl.stream[0].format[0].codec[0].url_info[0].host) {
+          host = json.data.playurl_info.playurl.stream[0].format[0].codec[0].url_info[0].host;
+          baseurl = json.data.playurl_info.playurl.stream[0].format[0].codec[0].base_url;
+          extra = json.data.playurl_info.playurl.stream[0].format[0].codec[0].url_info[0].extra;
+        }
+        roomurl = host + baseurl + extra;
+        navigator.clipboard.writeText(roomurl).catch(e => console.error(e))
+        GM_notification({
+          title: "解析成功",
+          image: "https://testingcf.jsdelivr.net/gh/mmyo456/BiliAnalysis-1@main/img/6.jpg",
+          text: "解析成功",
+          highlight: true,
+          silent: false,
+          timeout: 10000,
+          onclick: function () {
+          },
+          ondone() {
+          }
+        })
+      }
+    };
+  }
 })();
