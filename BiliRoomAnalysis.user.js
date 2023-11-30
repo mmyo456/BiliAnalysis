2d7415528fee2e9f572b2d09115b2e8c941240f1
Miro <70092715+529565622@users.noreply.github.com>
2023-11-30 19:47:44 +0800

    Update BiliRoomAnalysis.user.js

--- BiliRoomAnalysis.user.js
+++ BiliRoomAnalysis.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BiliBili直播间本地解析(Miro)
 // @namespace    https://bbs.tampermonkey.net.cn/
-// @version      0.1.1
+// @version      0.1.2
 // @description  try to take over the world!
 // @author       Miro(https://vrchat.com/home/user/usr_20b8e0e4-9e16-406a-a61d-8a627ec1a2e3)
 // @match        https://live.bilibili.com/*
@@ -9,7 +9,6 @@
 // @updateURL    https://raw.githubusercontent.com/529565622/BiliAnalysis/main/BiliRoomAnalysis.user.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_notification
-// @run-at       document-idle
 // @require      https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.2.1/jquery.min.js
 // ==/UserScript==
 
@@ -49,26 +48,25 @@
         httpRequest.onreadystatechange = function () {
             if (httpRequest.readyState == 4 && httpRequest.status == 200) {
                 var json = JSON.parse(httpRequest.responseText);
-                if (json.data.playurl_info.playurl.stream.length > 1) {
-                    var host = json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].host
-                    console.log(host);
-                    var baseurl = json.data.playurl_info.playurl.stream[1].format[0].codec[0].base_url
-                    console.log(baseurl);
-                    var extra = json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].extra
-                    console.log(extra);
-                    var roomurl = host + baseurl + extra
-                    navigator.clipboard.writeText(roomurl).catch(e => console.error(e))
-                } else {
-                    var host = json.data.playurl_info.playurl.stream[0].format[0].codec[0].url_info[0].host
-                    console.log(host);
-                    var baseurl = json.data.playurl_info.playurl.stream[0].format[0].codec[0].base_url
-                    console.log(baseurl);
-                    var extra = json.data.playurl_info.playurl.stream[0].format[0].codec[0].url_info[0].extra
-                    console.log(extra);
-                    var roomurl = host + baseurl + extra
-                    navigator.clipboard.writeText(roomurl).catch(e => console.error(e))
+                var host = "";
+                var baseurl = "";
+                var extra = "";
+                var roomurl
+                if (json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].host) {
+                  host = json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].host;
+                  baseurl = json.data.playurl_info.playurl.stream[1].format[1].codec[0].base_url;
+                  extra = json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].extra;
+                } else if (json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].host) {
+                  host = json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].host;
+                  baseurl = json.data.playurl_info.playurl.stream[1].format[0].codec[0].base_url;
+                  extra = json.data.playurl_info.playurl.stream[1].format[0].codec[0].url_info[0].extra;
+                } else if (json.data.playurl_info.playurl.stream[0].format[0].codec[0].url_info[0].host) {
+                  host = json.data.playurl_info.playurl.stream[0].format[0].codec[0].url_info[0].host;
+                  baseurl = json.data.playurl_info.playurl.stream[0].format[0].codec[0].base_url;
+                  extra = json.data.playurl_info.playurl.stream[0].format[0].codec[0].url_info[0].extra;
                 }
-
+                roomurl = host + baseurl + extra;
+                navigator.clipboard.writeText(roomurl).catch(e => console.error(e))
                 GM_notification({
                     title: "解析成功",
                     image: "https://testingcf.jsdelivr.net/gh/mmyo456/BiliAnalysis-1@main/img/6.jpg",
